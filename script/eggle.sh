d383de4bd3ef321cd2f30e28227122bffb066a31
gbox3d <gbox3d@gmail.com>
2020-12-29 16:45:27 +0900

    ...

--- script/eggle.sh
+++ script/eggle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /Users/gbox3d/Desktop/work/myproject/neuronetwork/neuroject/yolov5 && python detect.py --source=0 --weights=weights/toy_set/best.pt --conf=0.25
+cd /Users/gbox3d/Desktop/work/myproject/neuronetwork/neuroject/yolov5 && python detect.py --source=0 --weights=weights/toy_set/m/best.pt --conf=0.25
