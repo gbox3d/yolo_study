470592407dd85d18d082f85ea4472a9223323a22
gbox3d <gbox3d@gmail.com>
2021-05-03 12:03:40 +0900

    ...

--- models/download_weights.sh
+++ models/download_weights.sh
@@ -2,6 +2,8 @@
 # Download latest models from https://github.com/ultralytics/yolov5/releases
 
 python3 - <<EOF
+import sys
+sys.path.append('../')
 from libs.utils.google_utils import attempt_download
 
 for x in ['s', 'm', 'l', 'x']:
