612fc1361b512fa8add52643848a214dfe2603bb
gbox3d <gbox3d@gmail.com>
2022-07-26 12:15:45 +0900

    last line sytax error fix

--- ex01_gpu_test.py
+++ ex01_gpu_test.py
@@ -6,8 +6,5 @@ from IPython.display import Image, clear_output  # to display images
 clear_output()
 
 #%%
-
 print('Setup complete. Using torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
-print(f'torch vision {torchvision.__version__}')
-# %%
-0
+print(f'torch vision {torchvision.__version__}')# %%
