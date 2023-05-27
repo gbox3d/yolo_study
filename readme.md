a6959f55a8990cf27d1b6b96915ccf42e64da8b5
gbox3d <gbox3d@gmail.com>
2023-05-27 18:00:58 +0900

    yolo format tester

--- readme.md
+++ readme.md
@@ -13,4 +13,8 @@ wget https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/
 unzip coco128.zip
 ``` 
 
+## 실습용 데이터셋 받는 방법
 
+```bash
+python yolo_format_test.py --basepath /home/gbox3d/work/dataset/test/ --imageFile 150759820_900615604088631_2924655063235727439_n.jpg --data ./datasets/coco128.yaml
+```
--- yolo_format_test.py
+++ yolo_format_test.py
@@ -1,19 +1,18 @@
-#%%
+#%% Import packages
+import argparse
 import cv2 as cv
 import os
-import numpy as np
-from IPython.display import display
-import PIL.ImageFont as ImageFont
-import PIL.ImageDraw as ImageDraw
-import PIL.ImageColor as ImageColor
-import PIL.Image as Image
-
 import yaml
 
-#%%
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description='Draw bounding boxes on images using labels.')
+parser.add_argument('--basepath', required=True, help='The basepath of your files.')
+parser.add_argument('--imageFile', required=True, help='The image file to draw bounding boxes on.')
+parser.add_argument('--data', required=True, help='The path of the yaml file.')
+args = parser.parse_args()
 
 # Read coco128.yaml file
-yaml_path = './datasets/coco128.yaml'
+yaml_path = args.data
 with open(yaml_path, 'r') as file:
     data = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -21,27 +20,23 @@ with open(yaml_path, 'r') as file:
 label_names = data['names']
 
 # Print label names
-for label_id, label_name in label_names.items():
-    print(f"Label ID: {label_id}, Label Name: {label_name}")
-    
-#%%
+#for label_id, label_name in label_names.items():
+#    print(f"Label ID: {label_id}, Label Name: {label_name}")
 
-# Read image
-image_path = os.path.join('./datasets/coco128', 'images/train2017/000000000036.jpg')
+#%% Read image
+image_path = os.path.join(args.basepath, 'src', args.imageFile)
 image = cv.imread(image_path)
 image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-# Display the original image
-display(Image.fromarray(image_rgb))
-
-#%%
+# get file name without extension
+label_file = os.path.splitext(args.imageFile)[0] + '.txt'
 # Read YOLO labels
-label_path = os.path.join('./datasets/coco128', 'labels/train2017/000000000036.txt')
+label_path = os.path.join(args.basepath, 'labels', label_file)
 
 with open(label_path, 'r') as file:
     labels = file.readlines()
 
-# Draw labeled boxes on the image
+#%% Draw labeled boxes on the image
 image_height, image_width, _ = image.shape
 
 for label in labels:
@@ -61,7 +56,7 @@ for label in labels:
     label_position = (x_min, y_min - 10)
     cv.putText(image_rgb, label_text, label_position, cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     
-# Display the image with labeled boxes
-display(Image.fromarray(image_rgb))
-
-# %%
+#%% Display the image
+# Save the image with labeled boxes
+output_path = os.path.join('./', 'output', args.imageFile)
+cv.imwrite(output_path, cv.cvtColor(image_rgb, cv.COLOR_RGB2BGR))
