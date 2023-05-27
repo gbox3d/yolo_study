4d7bed0a949765841c31ec250f155b8d4dfb9120
gbox3d <gbox3d@gmail.com>
2023-05-27 21:30:45 +0900

    ...

--- v8/ex03_02_posecam.py
+++ v8/ex03_02_posecam.py
@@ -1,13 +1,23 @@
-import cv2
+#%%
+import cv2 as cv
 from ultralytics import YOLO
 import time
 
+
+from IPython.display import display
+import PIL.ImageFont as ImageFont
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageColor as ImageColor
+import PIL.Image as Image
+
+
+
 # Load the YOLOv8 model
-model = YOLO('yolov8n-pose.pt')
+model = YOLO('yolov8s-pose.pt')
 
 # Open the default camera
-cap = cv2.VideoCapture(0)
-
+cap = cv.VideoCapture(0)
+#%%
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -16,29 +26,26 @@ while cap.isOpened():
         start_time = time.time()
         
         # Run YOLOv8 inference on the frame
-        results = model(frame)
+        results = model(frame, conf=0.5,verbose=False)  # predict on an image
         
-        # Get the keypoints and bounding boxes for each person
-        for person in results:
-            keypoints = person['keypoints']
-            boxes = person['boxes']
-            
-            # Draw keypoints and bounding boxes on the frame
-            for keypoint in keypoints:
-                cv2.circle(frame, (keypoint[0], keypoint[1]), 3, (0, 0, 255), -1)
-            cv2.rectangle(frame, (boxes[0], boxes[1]), (boxes[2], boxes[3]), (255, 0, 0), 2)
+        for det in results:
+            for kpts in det.keypoints.data:
+                # Each keypoint is a 2D tensor
+                for kpt in kpts:
+                    x, y, _ = map(int, kpt)
+                    cv.circle(frame, (x, y), 5, (0, 0, 255), -1)
         
         end_time = time.time()
         fps = 1 / (end_time - start_time)
         # print("FPS :", fps)
         
-        cv2.putText(frame, "FPS :"+str(int(fps)), (10, 50), cv2.FONT_HERSHEY_COMPLEX, 1.2, (255, 0, 255), 1, cv2.LINE_AA)
+        cv.putText(frame, "FPS :"+str(int(fps)), (10, 50), cv.FONT_HERSHEY_COMPLEX, 1.2, (255, 0, 255), 1, cv.LINE_AA)
         
         # Display the annotated frame
-        cv2.imshow("YOLOv8 Inference", frame)
+        cv.imshow("YOLOv8 Inference", frame)
         
         # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         # Break the loop if the end of the video is reached
@@ -46,4 +53,6 @@ while cap.isOpened():
 
 # Release the video capture object and close the display window
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
+
+# %%
